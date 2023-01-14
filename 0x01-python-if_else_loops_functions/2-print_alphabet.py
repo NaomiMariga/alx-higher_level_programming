314bfabba1e266db0b26d2ef7808cd7957e9f644
NaomiMariga <kaburamariga@gmail.com>
2023-01-14 03:24:23 -0800

    ascii alphabets

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''
+# chr() converts integers to their ascii values
+# end="" prevents the loop to move to the next line
+and hence prints in 1 line
+'''
+for i in range(97, 123):
+    print(chr(i), end="")
