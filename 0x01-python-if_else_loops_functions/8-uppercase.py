fc4c1f1d2054d2d5af1170063d0350e190c8d14b
NaomiMariga <kaburamariga@gmail.com>
2023-01-14 11:49:22 -0800

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        number = ord(letter)
+        if number >= 97 and number <= 122:
+            letter = number - 32
+            letter = chr(letter)
+            print("{}".format(letter), end="")
+        else:
+            letter = chr(number)
+            print("{}".format(letter), end="")
+    print()
