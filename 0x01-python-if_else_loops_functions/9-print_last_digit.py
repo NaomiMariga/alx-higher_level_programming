8fe971ad370a4d3aa9fcf4c232cf1162fe9a897a
NaomiMariga <kaburamariga@gmail.com>
2023-01-15 01:23:43 -0800

    last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number > 0:
+        lastdigit = number % 10
+        print(lastdigit, end="")
+    elif number == 0:
+        lastdigit = 0
+        print(lastdigit, end="")
+    elif number < 0:
+        number *= -1
+        lastdigit = number % 10
+        print(lastdigit, end="")
