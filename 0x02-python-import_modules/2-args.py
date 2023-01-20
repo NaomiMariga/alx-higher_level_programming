005e1bb788044bfb1ad512f3780f64ad61c4e216
NaomiMariga <kaburamariga@gmail.com>
2023-01-20 13:19:05 -0800

    Dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def print_args(arguments):
+    number = len(arguments)
+
+    if number == 0:
+        print("{} arguments:".format(number))
+    elif number == 1:
+        print("{} argument:".format(number))
+        print("{}: {}".format(number, arguments[0]))
+    else:
+        print("{} arguments:".format(number))
+        for argument in range(number):
+            print("{}: {}".format(argument + 1, arguments[argument]))
+
+
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    print_args(argv)
