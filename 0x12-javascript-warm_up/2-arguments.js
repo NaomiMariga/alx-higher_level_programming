b1a8291ec49ba13edfe2f5a638785ca3af331cd1
NaomiMariga <kaburamariga@gmail.com>
2023-01-28 21:38:53 +0000

    use require() instead of import

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
+const processes = require('process');
+const argv = processes.argv;
 if (argv.length === 2) {
   console.log('No Argument');
 } else if (argv.length === 3) {
   console.log('Argument found');
-} else if (argv.length < 3) {
+} else if (argv.length > 3) {
   console.log('Arguments found');
 }
