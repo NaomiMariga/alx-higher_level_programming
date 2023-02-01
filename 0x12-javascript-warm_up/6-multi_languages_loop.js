c02c700eecef7f4cd5f29bba191f6287b328cde4
NaomiMariga <kaburamariga@gmail.com>
2023-02-01 08:44:22 +0000

    chhange file directory

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrayLength = myStrings.length;
+for (let i = 0; i < arrayLength; i++) {
+  console.log(myStrings[i]);
+}
