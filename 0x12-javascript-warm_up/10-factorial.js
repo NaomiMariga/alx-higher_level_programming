16967f919eb67ef35ecfa75cc1cdc87f490c13eb
NaomiMariga <kaburamariga@gmail.com>
2023-02-10 10:15:26 +0000

    ===

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,7 +5,7 @@ let argv = processes.argv;
 function factorial (n) {
   if (isNaN(n)) {
     return 1;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
   } else {
     return n * factorial(n - 1);
