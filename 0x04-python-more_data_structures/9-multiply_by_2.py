9b17de6903b1c74ab6b515c420b203499fd77fc7
NaomiMariga <kaburamariga@gmail.com>
2023-02-26 06:14:13 -0800

    Fix: product

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 def multiply_by_2(a_dictionary):
     results = {}
     for key, value in a_dictionary.items():
-        if value % 2 == 0:
-            results.update({key: value})
+        prod = value * 2
+        results.update({key: prod})
     return results
