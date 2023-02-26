6157134d1fc30807715a3638ea6b26333f7aa4f3
NaomiMariga <kaburamariga@gmail.com>
2023-02-26 06:38:53 -0800

    max in a dict

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        values = []
+        keys = []
+        for key, value in a_dictionary.items():
+            values.append(value)
+            keys.append(key)
+        maxnum = max(values)
+        maxnum_pos = values.index(maxnum)
+        key = keys[maxnum_pos]
+        return key
+    else:
+        return None
